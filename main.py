3e70bb3a7b5438c696806768d42ebfba1f4c3fbc
AamwaA <splitworlds.amwa@gmail.com>
2025-05-31 20:09:12 +0200

    update

--- main.py
+++ main.py
@@ -23,17 +23,21 @@ while force == 0:
         force = int(result)
     except:
        print("Veuillez entrer un chiffre")
-while force > 50:
+while force > 100:
     result = input(prompt)
     try:
         force = int(result)
     except:
         print("Un chiffre cohérent svp...")
 
+'''dv_ennemi = {"gobelin": 1, "orc": 2, "troll": 3, "géant": 4}
+
 print("Vous voulez maintenant combattre un ennemi")
-dv_ennemi = int(input("c'est tout à votre honneur, mais sur une échelle de 1 à 4 comment estimez une menace a votre mesure!"))
-pv_ennemi = random.randrange(50, 100, 5) * dv_ennemi
-print("très bien ! vous allez donc vous battre contre un ennemi à " + str(pv_ennemi) + " points de vie.") 
+dv_ennemi = str(input("c'est tout à votre honneur, vous pouvez choisir entre un gobelin, un orc, un troll ou un géant. lequel vous semble a votre mesure?"))
+pv_ennemi = random.randrange(50, 100, 5) * int(dv_ennemi)
+print("très bien ! vous allez donc vous battre contre un " + str(dv_ennemi) + " ,un ennemi à " + int(pv_ennemi) + " points de vie.") '''
+
+pv_ennemi = 200
 
 force_ennemi = 20
 
@@ -41,7 +45,7 @@ force_ennemi = 20
 # faire une boucle pour utiliser des potions
 # faire une condition de victoire et de défaite
 
-coup = force_ennemi * dv_ennemi
+coup = force_ennemi * 1 #dv_ennemi
 dv_perdus = 0
 
 while pv_ennemi > 0:
@@ -64,17 +68,29 @@ while pv_ennemi > 0:
     if pv_ennemi <= 0:
         print("vous avez vaincu votre ennemi")
 
+pv_potion = random.randrange(10, 101, 10)
+loot_ennemi = ("hache", "épée", "armure", "bourse")
 
-'''print("Que voulez vous faire?")
-print(" 1 - boire une potion")
-print(" 2 - fouiller votre enemi")
-print(" 3 - partir d'ici")
-choice = input("Saisissez votre choix")
-if choice == '1':
+while pv_ennemi <= 0:
+    print("Que voulez vous faire?")
+    print(" 1 - boire une potion")
+    print(" 2 - fouiller votre enemi")
+    print(" 3 - partir d'ici")
+    choice = input("Saisissez votre choix")
+    if choice == '1':
+        pv += pv_potion
+    if pv < 100:
+        pv = 100
+    print("vous avez récupéré " + str(pv_potion) + " points de vie. vous en avez désormais " + str(pv) + ".")
+    if choice == '2':
+        loot = random.choice(loot_ennemi)
+        print("vous avez récupérer sur votre ennemi une " + str(loot) + ".")
+    if choice == '3':
+        pv_ennemi = 200
+        print("vous décidez de quitter la pièce")
 
 
-
-if int(dv_ennemi) < 0:
+'''if int(dv_ennemi) < 0:
     pv_restant = int(dv_ennemi) - int(force)
 elif pv_restant < 0:
     pv = int(pv) - int(coup)
@@ -93,5 +109,7 @@ while pv_restant < 0:
 
 
 #if pv_ennemi == 0:
-    #print("Vous avez triomphé !")
-'''
+    #print("Vous avez triomphé !")'''
+
+
+
