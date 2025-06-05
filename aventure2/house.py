034a592515f209f659b701a39eda98e698835862
AamwaA <splitworlds.amwa@gmail.com>
2025-06-05 14:43:44 +0200

    changement du système

--- aventure2/house.py
+++ aventure2/house.py
@@ -3,26 +3,28 @@ import random
 
 
 print("La maison : Ceux des Angles")
-menace = 0
 
 ma_maison = {"chambre": 0, "couloir": 1, "toilettes": 1, "salle de bain": 2, "bureau": 5, "escaliers": 1, "salon": 2, "cuisine": 3, "entrée": 2, "cave": 4}
-for cle, valeur in ma_maison.items():
-    while menace == 0:
-        print("Vous êtes dans votre maison, un bruit infime mais très net vous laisse une sensation etrange. il y a quelqu'un chez...")
-        print("voici une liste des pièces de votre maison")
-        for cle in ma_maison.items():
-            print(cle)
-        choice = input("dans quelle pièce voulez-vous vous rendre? ")
-        for valeur in ma_maison.values():
-            if choice == cle:
-                menace += valeur
-        if menace <= 0:
-            print("vous vous retrouvez dans : " + str(choice) + " la menace s'est aggrandie, l'intensité de sa menace à augmenté de " + str(menace) + ".")
-            break
-    while menace <= 0:
-        print("l'ambiance de la maison à changé...")
-        
-        if menace <= 7:
+
+
+menace = 0
+while menace == 0:
+    print("Vous êtes dans votre maison, un bruit infime mais très net vous laisse une sensation etrange. il y a quelqu'un chez...")
+    print("voici une liste des pièces de votre maison")
+    for cle in ma_maison.keys():
+        print(cle)
+    choice = input("dans quelle pièce voulez-vous vous rendre? ")
+    for cle,valeur in ma_maison.items():
+        if choice == str(cle):
+            ma_maison[cle] = valeur
             menace += valeur
-            print("la menace s'est aggrandie, l'intensité de sa menace à augmenté de " + str(menace) + ".")
-            break
+    if menace >= 0:
+        print("vous vous retrouvez dans : " + str(choice) + " la menace s'est aggrandie, l'intensité de sa menace à augmenté de " + str(menace) + ".")
+        break
+while menace >= 0:
+    print("l'ambiance de la maison à changé...")
+    
+    if menace >= 5:
+        menace += valeur
+        print("la menace s'est aggrandie, l'intensité de sa menace à augmenté de " + str(menace) + ".")
+        break
--- main.py
+++ main.py
@@ -6,65 +6,78 @@ nom = input("Quel est votre nom champion?")
 
 print("Enchanté " + nom + "! " + " Très joli nom.")
 
-pv = 100
+race = {"humain": 5, "nain": 7, "elfe": 3}
+
+exp = 10
+niveau = math.floor(exp/10)
+
+for dv in race.values():
+    pvj = dv*50*niveau
+    forcej = (10-dv)*niveau
+
+print("il y a trois race dans notre monde")
+print("vous avez le choix de jouer :")
+for choix in race.keys():
+    print(choix)
+print("les nains ont plus de point de vie, mais les elfes infligent plus de dégats")
+print("les humains quand à eux, sont un bon entre deux")
+
+for choix, dv in race.items():
+    prompt = "Race?"
+    result = input(prompt)
+    if result == str(type):
+        pvj = dv * 50
+        forcej = (10 - dv) * 10
+print("en tant que " + result + ", vous avez " + str(pvj) + " points de vie et " + str(forcej) + " en force.")
+print("vous êtes de niveau " + str(niveau))
+
+
+# finir le calcul des pv et de la force de départ
+
 potion = random.randrange(2, 6, 1)
+pv_potion = 50
 
-print("Vous avez " + str(pv) + " Points de vie, c'est déjà bien et comme je suis sympa je vais vous donner " + str(potion) + " potions de soin.")
+print("C'est déjà bien et comme je suis sympa je vais vous donner " + str(potion) + " potions de soin.")
 
-print("Comme j'ai pas trop eût le temps de préparer ça, combien tu veux en Force?")
-force = 0
+monstres = {"gobelin": 1, "orc": 2, "troll": 3, "géant": 4}
 
-# Il y a surement un problème dans la boucle, j'aimerais qu'elle vérifie les deux try en même temps, pour que ça change selon ce qu'on met
+pv_ennemi = 0
+force_ennemi = 0
+            
+for type, fp in monstres.items():
+    print("Vous voulez maintenant combattre un ennemi")
+    if type == input("c'est tout à votre honneur, vous pouvez choisir entre un gobelin, un orc, un troll ou un géant. lequel vous semble a votre mesure?"):
+        pv_ennemi = fp * 100
+        force_ennemi = fp * 5
+    print("très bien ! vous allez donc vous battre contre un " + str(type) + " ,un ennemi à " + str(pv_ennemi) + " points de vie.")
 
-prompt = "Force?"
-while force == 0:
-    result = input(prompt)
-    try: 
-        force = int(result)
-    except:
-       print("Veuillez entrer un chiffre")
-while force > 100:
-    result = input(prompt)
-    try:
-        force = int(result)
-    except:
-        print("Un chiffre cohérent svp...")
-
-'''dv_ennemi = {"gobelin": 1, "orc": 2, "troll": 3, "géant": 4}
-
-print("Vous voulez maintenant combattre un ennemi")
-dv_ennemi = input("c'est tout à votre honneur, vous pouvez choisir entre un gobelin, un orc, un troll ou un géant. lequel vous semble a votre mesure?")
-pv_ennemi = random.randrange(50, 100, 5) * dv_ennemi.get(input)
-print("très bien ! vous allez donc vous battre contre un " + str(dv_ennemi) + " ,un ennemi à " + int(pv_ennemi) + " points de vie.")'''
-
-force_ennemi = 20
-
-pv_ennemi = 200
-
-# faire une condition de victoire et de défaite
-
-coup = force_ennemi * 1 #dv_ennemi
+coup = force_ennemi
 dv_perdus = 0
 
 while pv_ennemi > 0:
     print("Que voulez vous faire?")
     print(" 1 - attaquer")
-    print(" 2 - bloquer")
-    print(" 3 - fuir")
+    print(" 2 - boire une potion ")
     choice = input("Saisissez votre choix")
     if choice == '1':
-        pv_ennemi -= force
-        dv_perdus += force
+        pv_ennemi -= forcej
+        dv_perdus += forcej
         print("il reste " + str(pv_ennemi) + " points de vie à l'ennemi")
-        if dv_perdus >= 20:
+        if dv_perdus >= pv_ennemi/10:
             dv_perdus = 0
-            pv -= coup
+            pvj -= coup
             print("l'ennemi attaque, il vous reste" + str(pv) + " points de vie")
-    if pv <= 0:
+    if choice == '1':
+        pvj += pv_potion
+    if pvj > 100:
+        pvj = 100
+    print("vous avez récupéré " + str(pv_potion) + " points de vie. vous en avez désormais " + str(pv) + ".")
+    if pvj <= 0:
         print("Vous êtes mort")
         break
     if pv_ennemi <= 0:
         print("vous avez vaincu votre ennemi")
+    
 
 pv_potion = random.randrange(10, 101, 10)
 loot_ennemi = ("hache", "épée", "armure", "bourse")
@@ -77,7 +90,7 @@ while pv_ennemi <= 0:
     choice = input("Saisissez votre choix")
     if choice == '1':
         pv += pv_potion
-        if pv < 100:
+        if pv > 100:
             pv = 100
         print("vous avez récupéré " + str(pv_potion) + " points de vie. vous en avez désormais " + str(pv) + ".")
     if choice == '2':
@@ -87,27 +100,7 @@ while pv_ennemi <= 0:
         pv_ennemi = 200
         print("vous décidez de quitter la pièce")
 
-
-'''if int(dv_ennemi) < 0:
-    pv_restant = int(dv_ennemi) - int(force)
-elif pv_restant < 0:
-    pv = int(pv) - int(coup)
-else:
-    print("...")
-
-
-
-prompt = "attaquer?"
-while pv_restant < 0:
-    result = input(prompt)
-    try:
-        int(pv_restant) == int(dv_ennemi)
-    except:
-        print("Vous avez triomphé !")
-
-
-#if pv_ennemi == 0:
-    #print("Vous avez triomphé !")'''
+del monstres[monstre]
 
 
 
