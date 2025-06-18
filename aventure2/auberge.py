71f0d7ed6a0a94e34f70e5f51011e309c3b059a8
AamwaA <splitworlds.amwa@gmail.com>
2025-06-18 02:28:44 +0200

    focusing on first auberge function

--- aventure2/auberge.py
+++ aventure2/auberge.py
@@ -15,9 +15,9 @@ aventurier = {
 },
     "niveau": {
         1,
-        2,
-        3,
-        4,
+        5,
+        7,
+        9,
     },
     "nuités": {
         1,
@@ -25,6 +25,20 @@ aventurier = {
         3,
     }}
 
-def genPnj(nombre, dicAv):
-    locataires = liste(dicAv.keys())
+def Genaventurier():
+    race = random.choice[aventurier]["race"]
+    niveau = random.choice[aventurier]["niveau"]
+    nuites = random.choice(aventurier["nuités"])
+    pnj = list(race, niveau, nuites)
+    return pnj
+
+Genaventurier()
+
+print("Une passe derrière votre fenêtre,")
+print("La porte s'ouvre dans un grincement strident")
+print("C'est un aventurier en quête de gloire")
+print("Il a besoin de se reposer pour" + str(nuites) + " nuités das votre auberge")
+print("Aux trais de son visage et de sa carrure, vous reconnaisesz un " + str(race))
+print("grâce à votre expérience d'aventurier, vous l'estimez de niveau " + str(niveaux))
+    
     
