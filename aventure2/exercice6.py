4b8755e2b395e2474956cbb5c63cedd4c269f055
AamwaA <splitworlds.amwa@gmail.com>
2025-06-29 13:55:38 +0200

    sum more exercices

--- aventure2/exercice6.py
+++ aventure2/exercice6.py
@@ -31,17 +31,74 @@ class Carnet:
             for t in self.taches:
                 t.afficher()
         
-    def rechercher_tache(self, tache):
-            if tache in self.taches:
-                print(tache)
+    def rechercher_par_titre(self, titre):
+        for tache in self.taches:
+            if tache.titre == titre:
+                return tache
+        return None
 
 
+carnet_vide = False
 
 carnet = Carnet()
 carnet.ajouter_taches(Tache("manger", 5, "à venir"))
 carnet.ajouter_taches(Tache("ménage", 4, "à faire"))
 carnet.ajouter_taches(Tache("coder", 3, "en cours"))
 
-carnet.afficher_toutes()
 
-carnet.rechercher_tache("manger")
+
+while not carnet_vide:
+    print("================================")
+    print("1 - consulter les tâches")
+    print("2 - modifier une tâche")
+    print("3 - ajouter une tâche")
+    print("4 - supprimer une tâche")
+    print("5 - partir")
+    print("================================")
+    choix = input("Que souhaitez vous faire?")
+
+    if choix == "1":
+         carnet.afficher_toutes()
+    
+    if choix == "2":
+            choix = input("quelle tâche voulez-vous modifier?")
+            tache = carnet.rechercher_par_titre(choix)
+            if tache is None:
+                print("Tâche non trouvée.")
+            else:
+                if tache is not None:
+                    choix2 = input("voulez modifier le statut(1) ou la priorité(2) ou les deux(3)?")
+
+                if choix2 == "1":
+                    nouveau_statut = input("entrez un nouveau statut:")
+                    tache.changer_statut(nouveau_statut)
+        
+                if choix2 == "2":
+                    nouvelle_priorité = int(input("Entrez une nouvelle priorité : "))
+                    tache.changer_priorite(nouvelle_priorité)
+        
+                if choix2 == "3":
+                    nouveau_statut = input("entrez un nouveau statut:")
+                    tache.changer_statut(nouveau_statut)
+                    nouvelle_priorité = int(input("Entrez une nouvelle priorité : "))
+                    tache.changer_priorite(nouvelle_priorité) 
+
+
+    if choix == "3":
+         nom = input("quelle tache voulez vous ajouter")
+         priorité = int(input("quelle est sa priotité? (1->5)"))
+         statut = input("quel est son statut?")
+         carnet.ajouter_taches(Tache(nom, priorité, statut))
+    
+    if choix == "4":
+         choix = input("quelle tache voulez-vous supprimer?")
+         tache = carnet.rechercher_par_titre(choix)
+         if tache is None:
+                print("Tâche non trouvée.")
+         else:
+            carnet.supprimer_taches(tache)
+
+    if choix == "5":
+         carnet_vide = True
+
+
